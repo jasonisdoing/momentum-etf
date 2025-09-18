7bea4309615ee698455c52970e02d4d0982f973a
Jason <kimwootak@catholic.ac.kr>
2025-09-19 08:32:51 +0900

    APP_VERSION_TIME => APP_DATE_TIME

--- settings.py
+++ settings.py
@@ -18,6 +18,6 @@ BENCHMARK_TICKERS = {
     "aus": "IVV.AX",  # iShares S&P 500 (AUD)
 }
 
-APP_VERSION_TIME = os.environ.get("APP_VERSION_TIME", "2025-09-19-07")
+APP_DATE_TIME = "2025-09-19-08"
 
-APP_TYPE = os.environ.get("APP_TYPE", f"APP-{APP_VERSION_TIME}")
+APP_TYPE = os.environ.get("APP_TYPE", f"APP-{APP_DATE_TIME}")
