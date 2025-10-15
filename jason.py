184a296b7abc9de27c3314b97184f5e7bd5834a3
Jason <kimwootak@catholic.ac.kr>
2025-10-15 16:08:16 +0900

    디버그 코드 추가

--- jason.py
+++ jason.py
@@ -1,19 +1,7 @@
-import pandas as pd
 import yfinance as yf
 
-# yfinance를 통해 전체 기간 데이터 다운로드
-data = yf.download("463690.KS", period="max", progress=False, auto_adjust=False)
-print(data)
+ticker = yf.Ticker("^GSPC")
+data = ticker.history(period="1d")
+current_price = data["Close"].iloc[-1]
 
-# yfinance가 MultiIndex 컬럼을 반환하는 경우 단일 레벨로 정리
-if isinstance(data.columns, pd.MultiIndex):
-    data.columns = data.columns.get_level_values(0)
-    data = data.loc[:, ~data.columns.duplicated()]
-
-# 중복된 인덱스가 있을 경우 마지막 항목만 남김
-if not data.index.is_unique:
-    data = data[~data.index.duplicated(keep="last")]
-
-# 1. 상장일 업데이트
-first_trading_day = data.index.min().strftime("%Y-%m-%d")
-print(first_trading_day)
+print(f"S&P 500 (^GSPC) 현재 종가: {current_price}")
