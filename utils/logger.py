4e63c24d6047058cf5c229ca82687a3591cf8291
Jason <kimwootak@catholic.ac.kr>
2025-10-29 08:55:35 +0900

    2025-10-29 한국/호주 설정 업데이트

--- utils/logger.py
+++ utils/logger.py
@@ -16,7 +16,7 @@ except ImportError:
 
 load_dotenv()
 
-APP_VERSION = "2025-10-28-22"
+APP_VERSION = "2025-10-29-08"
 APP_LABEL = os.environ.get("APP_TYPE", f"APP-{APP_VERSION}")
 
 LOG_LEVEL = os.environ.get("APP_LOG_LEVEL", "INFO").upper()
