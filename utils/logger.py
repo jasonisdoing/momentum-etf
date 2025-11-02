ae242a734419b4697b094acf98acb942ec47ce7b
Jason <kimwootak@catholic.ac.kr>
2025-11-03 08:52:28 +0900

    호주 고정종목 변경

--- utils/logger.py
+++ utils/logger.py
@@ -16,7 +16,7 @@ except ImportError:
 
 load_dotenv()
 
-APP_VERSION = "2025-11-03-01"
+APP_VERSION = "2025-11-03-08"
 APP_LABEL = os.environ.get("APP_TYPE", f"APP-{APP_VERSION}")
 
 LOG_LEVEL = os.environ.get("APP_LOG_LEVEL", "INFO").upper()
