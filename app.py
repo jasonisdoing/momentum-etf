8effa498e29fd4f75e6d5a66e05648c0689fcd85
Jason <kimwootak@catholic.ac.kr>
2025-11-05 15:48:06 +0900

    [Admin] 종목 정보 오픈

--- app.py
+++ app.py
@@ -298,14 +298,14 @@ def main() -> None:
         )
     )
 
-    # pages.append(
-    #     page_cls(
-    #         "app_pages/stocks.py",
-    #         title="[Admin] 종목 정보",
-    #         icon="📊",
-    #         url_path="stocks",
-    #     )
-    # )
+    pages.append(
+        page_cls(
+            "app_pages/stocks.py",
+            title="[Admin] 종목 정보",
+            icon="📊",
+            url_path="stocks",
+        )
+    )
 
     # pages.append(
     #     page_cls(
