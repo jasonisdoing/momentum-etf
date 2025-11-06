d3a54ec17ec42b658b6fd4e7256a434cb9585f63
Jason <kimwootak@catholic.ac.kr>
2025-11-06 12:27:09 +0900

    필요없는 항목 제거

--- utils/logger.py
+++ utils/logger.py
@@ -16,7 +16,7 @@ except ImportError:
 
 load_dotenv()
 
-APP_VERSION = "2025-11-06-11"
+APP_VERSION = "2025-11-06-12"
 APP_LABEL = os.environ.get("APP_TYPE", f"APP-{APP_VERSION}")
 
 LOG_LEVEL = os.environ.get("APP_LOG_LEVEL", "INFO").upper()
