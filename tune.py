3a9120e7e7b9941238ed1faa180490677ea9fd67
Jason <kimwootak@catholic.ac.kr>
2025-12-15 12:01:20 +0900

    계좌 튜닝 업데이트

--- tune.py
+++ tune.py
@@ -18,7 +18,7 @@ TUNING_CONFIG: dict[str, dict] = {
         "MA_TYPE": ["EMA"],
         "REPLACE_SCORE_THRESHOLD": [0, 1, 2, 3, 4, 5],
         "STOP_LOSS_PCT": [5, 6, 7, 8, 9, 10],
-        "OVERBOUGHT_SELL_THRESHOLD": [80, 82, 84, 86, 88, 90],
+        "OVERBOUGHT_SELL_THRESHOLD": [84, 86, 88, 90],
         "TRAILING_STOP_PCT": [0],
         "COOLDOWN_DAYS": [0, 1, 2, 3],
         "CORE_HOLDINGS": [],
@@ -29,7 +29,7 @@ TUNING_CONFIG: dict[str, dict] = {
         "MA_RANGE": [20, 25, 30, 35, 40, 45, 50],  # 범위가 넓어지면 과최적화 위험 증가
         "MA_TYPE": ["EMA"],
         "REPLACE_SCORE_THRESHOLD": [0, 1, 2, 3, 4, 5],
-        "STOP_LOSS_PCT": [8],
+        "STOP_LOSS_PCT": [5],
         "OVERBOUGHT_SELL_THRESHOLD": [85, 86, 87, 88, 89, 90, 91, 92, 93],
         "TRAILING_STOP_PCT": [0],
         "COOLDOWN_DAYS": [0, 1, 2, 3],
