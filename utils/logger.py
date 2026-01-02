bd4a1259db47f232d46c8120e987bd18e7bd9af0
Jason <kimwootak@catholic.ac.kr>
2026-01-02 13:01:01 +0900

    🇰🇷 한국 - 🚗 자율주행로 고정

--- utils/logger.py
+++ utils/logger.py
@@ -16,7 +16,7 @@ except ImportError:
 
 load_dotenv()
 
-APP_VERSION = "2026-01-02-12"
+APP_VERSION = "2026-01-02-13"
 APP_LABEL = os.environ.get("APP_TYPE", f"APP-{APP_VERSION}")
 
 LOG_LEVEL = os.environ.get("APP_LOG_LEVEL", "INFO").upper()
