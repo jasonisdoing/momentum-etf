ac7bd54d0c6bbb7a78dab4cc7803dd0649cb5289
Jason <kimwootak@catholic.ac.kr>
2026-01-05 17:23:23 +0900

    일부 종목 제거 및 추가

--- tune.py
+++ tune.py
@@ -18,7 +18,7 @@ ACCOUNT_TUNING_CONFIG = {
     "kor": {
         "PORTFOLIO_TOPN": [10],
         "REPLACE_SCORE_THRESHOLD": [0],
-        "MA_RANGE": [30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
+        "MA_RANGE": [20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
         # "MA_TYPE": ["SMA", "EMA", "HMA"],
         "MA_TYPE": ["EMA"],
     },
@@ -26,7 +26,7 @@ ACCOUNT_TUNING_CONFIG = {
     "aus": {
         "PORTFOLIO_TOPN": [6],
         "REPLACE_SCORE_THRESHOLD": [0],
-        "MA_RANGE": [30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
+        "MA_RANGE": [20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
         # "MA_TYPE": ["SMA", "EMA", "HMA"],
         "MA_TYPE": ["SMA"],
     },
@@ -34,7 +34,7 @@ ACCOUNT_TUNING_CONFIG = {
     "us": {
         "PORTFOLIO_TOPN": [5],
         "REPLACE_SCORE_THRESHOLD": [0],
-        "MA_RANGE": [30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200],
+        "MA_RANGE": [20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200],
         # "MA_RANGE": [50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100],
         # "MA_TYPE": ["SMA", "EMA", "HMA"],
         "MA_TYPE": ["HMA"],
