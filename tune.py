22244bdec5e72c65e0feedd366cbd51bd76a0ace
Jason <kimwootak@catholic.ac.kr>
2026-01-09 11:17:43 +0900

    kor_kr 6 종목으로 튜닝

--- tune.py
+++ tune.py
@@ -16,7 +16,7 @@ from utils.logger import get_app_logger
 ACCOUNT_TUNING_CONFIG = {
     # 🇰🇷 국내 ETF: PORTFOLIO_TOPN 테스트 중
     "kor_kr": {
-        "PORTFOLIO_TOPN": [5],
+        "PORTFOLIO_TOPN": [6],
         "REPLACE_SCORE_THRESHOLD": [0],
         # 1개월 ~ 3개월
         "MA_RANGE": [20, 30, 40, 50, 60],
