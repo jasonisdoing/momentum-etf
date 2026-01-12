c064e02d9705c53025efc77cafc7e3c430df05c4
Jason <kimwootak@catholic.ac.kr>
2026-01-12 23:04:30 +0900

    예외 카테고리 중복 허용 안하는 문제 해결

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ MAX_PER_CATEGORY = 1
 
 # 카테고리 중복 제한에서 예외되는 카테고리 목록
 # 이 카테고리들은 여러 종목을 동시에 보유할 수 있음
-CATEGORY_EXCEPTIONS = ["기타"]
+CATEGORY_EXCEPTIONS = ["예외"]
 
 # 네이버 금융 API 설정
 NAVER_FINANCE_ETF_API_URL = "https://finance.naver.com/api/sise/etfItemList.nhn"
