330f8519ea56eefc3adb37a57a7e0a933b9bed55
Jason <kimwootak@catholic.ac.kr>
2026-01-16 21:47:59 +0900

    kor 계좌 benchmark 변경, PORTFOLIO_TOPN 줄여서 재튜닝

--- tune.py
+++ tune.py
@@ -16,22 +16,22 @@ from utils.logger import get_app_logger
 ACCOUNT_TUNING_CONFIG = {
     # 🇰🇷 국내 ETF: PORTFOLIO_TOPN 테스트 중
     "kor_kr": {
-        "PORTFOLIO_TOPN": [6, 7, 8],
+        "PORTFOLIO_TOPN": [4, 5, 6],
         "REPLACE_SCORE_THRESHOLD": [0],
         # 1개월 ~ 3개월
-        "MA_RANGE": [20, 30, 40, 50, 60],
+        # "MA_RANGE": [20, 30, 40, 50, 60],
         # 2개월 ~ 6개월
-        # "MA_RANGE": [40, 50, 60, 70, 80, 90, 100, 110, 120],
+        "MA_RANGE": [40, 50, 60, 70, 80, 90, 100, 110, 120],
         # "MA_TYPE": ["SMA", "EMA", "HMA"],
         "MA_TYPE": ["EMA"],
     },
     "kor_us": {
-        "PORTFOLIO_TOPN": [6, 7, 8],
+        "PORTFOLIO_TOPN": [4, 5, 6],
         "REPLACE_SCORE_THRESHOLD": [0],
         # 1개월 ~ 3개월
-        "MA_RANGE": [20, 30, 40, 50, 60],
+        # "MA_RANGE": [20, 30, 40, 50, 60],
         # 2개월 ~ 6개월
-        # "MA_RANGE": [40, 50, 60, 70, 80, 90, 100, 110, 120],
+        "MA_RANGE": [40, 50, 60, 70, 80, 90, 100, 110, 120],
         # "MA_TYPE": ["SMA", "EMA", "HMA"],
         "MA_TYPE": ["EMA"],
     },
