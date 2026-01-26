f7a396aaa72bef750d33346ee8cefc2e939c76b0
Jason <kimwootak@catholic.ac.kr>
2026-01-26 19:38:46 +0900

    kor_us 희토류, 금 카테고리 분리

--- tune.py
+++ tune.py
@@ -29,7 +29,7 @@ ACCOUNT_TUNING_CONFIG = {
         "MA_TYPE": ["SMA", "EMA", "HMA"],
     },
     "kor_us": {
-        "PORTFOLIO_TOPN": [4],
+        "PORTFOLIO_TOPN": [4, 5],
         "REPLACE_SCORE_THRESHOLD": [0],
         # 1개월 ~ 3개월: 급락 혹은 급등시
         # "MA_RANGE": [20, 30, 40, 50, 60],
