7831c54c3ba8a114ce43c9a811992a5b9a1c941e
Jason <kimwootak@catholic.ac.kr>
2026-02-09 15:50:02 +0900

    kor_us 5종목 튜닝

--- tune.py
+++ tune.py
@@ -22,7 +22,7 @@ ACCOUNT_TUNING_CONFIG = {
         "MA_TYPE": ["SMA", "EMA", "WMA", "DEMA", "TEMA", "HMA"],
     },
     "kor_us": {
-        "PORTFOLIO_TOPN": [4, 5],
+        "PORTFOLIO_TOPN": [5],
         "REPLACE_SCORE_THRESHOLD": [0],
         "MA_RANGE": [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200],
         "MA_TYPE": ["SMA", "EMA", "WMA", "DEMA", "TEMA", "HMA"],
