5532247bffedae199aa98feadc74c41cc440f906
Jason <kimwootak@catholic.ac.kr>
2026-02-24 08:29:45 +0900

    한국계좌 icon 변경

--- utils/logger.py
+++ utils/logger.py
@@ -16,7 +16,7 @@ except ImportError:
 
 load_dotenv()
 
-APP_VERSION = "2026-02-23-23"
+APP_VERSION = "2026-02-24-08"
 APP_LABEL = os.environ.get("APP_TYPE", f"APP-{APP_VERSION}")
 
 LOG_LEVEL = os.environ.get("APP_LOG_LEVEL", "INFO").upper()
