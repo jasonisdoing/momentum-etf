9db02dd96e38b6324abf163c9db2f05269767aed
Jason <kimwootak@catholic.ac.kr>
2026-04-19 10:30:33 +0900

    백테스트 핵심 로직을 core/strategy/backtest.py로 분리, backtest.py에는 설정과 호출만 유지

--- core/strategy/__init__.py
+++ core/strategy/__init__.py
@@ -2,8 +2,10 @@
 
 from __future__ import annotations
 
+from .backtest import run_backtest
 from .scoring import calculate_maps_score
 
 __all__ = [
     "calculate_maps_score",
+    "run_backtest",
 ]
