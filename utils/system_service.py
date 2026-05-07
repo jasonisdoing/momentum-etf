27998bdb0616db84c9faf03a5b3c76089b120b99
Jason <kimwootak@catholic.ac.kr>
2026-05-07 19:08:41 +0900

    가격 캐시 cron을 매시 0분/30분 (월~토)으로 강화

--- utils/system_service.py
+++ utils/system_service.py
@@ -45,7 +45,7 @@ SCHEDULE_ROWS = [
         "key": "cache_refresh",
         "job": "가격 캐시 업데이트",
         "target": "모든 종목/포트폴리오 변동",
-        "cadence": "월~토 05:30 ~ 17:30 매시 30분 KST",
+        "cadence": "월~토 24시간 매시 0분/30분 KST",
         "command": "python scripts/stock_price_cache_updater.py",
     },
     {
